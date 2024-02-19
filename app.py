6878de94039d74d6b5794063edda44b437000e2e
Rigvedsarougi <117500784+Rigvedsarougi@users.noreply.github.com>
2024-02-19 11:27:09 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+from io import BytesIO
+from app_functions import process_audio_files
+
+app = Flask(__name__)
+
+@app.route('/audio-fraud-detection', methods=['POST'])
+def audio_fraud_detection():
+    if 'audio_files' not in request.files:
+        return jsonify({"error": "No audio files uploaded"}), 400
+    
+    audio_files = request.files.getlist('audio_files')
+    
+    keywords = [
+        'Global',
+        'HANA',
+        'Server',
+        'Software'
+    ]
+
+    results = process_audio_files(audio_files, keywords)
+    return jsonify(results)
+
+if __name__ == "__main__":
+    app.run(debug=True)
